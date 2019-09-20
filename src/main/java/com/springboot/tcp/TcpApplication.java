cf4cc80386355c847a66e4b98efa00dc4c44726c
greensJadeSoup <1252091009@qq.com>
2019-09-20 13:52:56 +0800

    1.tcp测试工具连接服务器ip，端口6379，服务器接收后将ASCII码显示到控制台 2.大改，现在可以正常运行了，Server2为非springboot版本

--- src/main/java/com/springboot/tcp/TcpApplication.java
+++ src/main/java/com/springboot/tcp/TcpApplication.java
@@ -7,20 +7,17 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import com.springboot.tcp.test.Server;
 
-@Slf4j
-@SpringBootApplication
-public class TcpApplication extends SpringBootServletInitializer {
+import java.io.IOException;
 
-    public static void main(String[] args)
+@SpringBootApplication
+public class TcpApplication extends SpringBootServletInitializer
+{
+
+    public static void main(String[] args) throws IOException
     {
         SpringApplication.run(TcpApplication.class, args);
         Server server = new Server();
-        try {
-            server.getServerDemo();//调用开启服务器
-        } catch (Exception e) {
-            log.error(e.getMessage());//记录日志
-//            e.printStackTrace();
-        }
+        server.getServerDemo();//调用开启服务器
     }
 
     @Override
--- src/main/java/com/springboot/tcp/test/Server.java
+++ src/main/java/com/springboot/tcp/test/Server.java
@@ -2,57 +2,78 @@ package com.springboot.tcp.test;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
 
 @Slf4j
 public class Server
 {
-    public void getServerDemo() throws Exception
-    {
-
-        ServerSocket server = new ServerSocket(6973);//设置服务器端口号
-        System.out.println("服务器开启,等到客户端链接------------------   ");//服务器开启时控制台输出
-        while (true) {
-            Socket socket = server.accept();
-            new Agent(socket).start();
+    public void getServerDemo() throws IOException {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        try {
+            //建立服务器的Socket，并设定一个监听的端口PORT
+            serverSocket = new ServerSocket(6379);
+            //由于需要进行循环监听，因此获取消息的操作应放在一个while大循环中
+            while(true){
+                try {
+                    //建立跟客户端的连接
+                    socket = serverSocket.accept();
+                } catch (Exception e) {
+                    System.out.println("建立与客户端的连接出现异常");
+                    e.printStackTrace();
+                }
+                ServerThread thread = new ServerThread(socket);
+                thread.start();
+            }
+        } catch (Exception e) {
+            System.out.println("端口被占用");
+            e.printStackTrace();
+        } finally {
+            serverSocket.close();
         }
     }
 
-    class Agent extends Thread
+    class ServerThread extends Thread
     {
-        Socket socket = null;
+        private Socket socket ;
+        InputStream inputStream;
+        OutputStream outputStream;
 
-        public Agent(Socket soc)
-        {
-            this.socket = soc;
+        public  ServerThread(Socket socket){
+            this.socket=socket;
         }
-
-        public void run()
-        {
+        public void run(){
             try {
-                InputStream in = socket.getInputStream();
-                OutputStream out = socket.getOutputStream();
-                out.write("welcome to Server!\0".getBytes());//给客户端返回信息告知其链接成功
-                out.flush();
-                byte[] bytes = null;
                 while (true){
-                    bytes = new byte[100];//大小可更改
-                    in.read(bytes);
-                    for (byte str:bytes) {
-                        System.out.println(str);
-                    }
-                    //System.out.println("客户端来消息"+ bytes[0]);
-                    System.out.println("客户端来消息"+ Arrays.toString(bytes));//由于这里是跟开发板做链接所以全部都必须输入输出为二进制才可以,所以无论传输的是什么都要转换成二进制的形式
-                    log.info(Arrays.toString(bytes));      //将客户的消息记录日志
+                    //接收客户端的消息并打印
+                    System.out.println(socket);
+                    inputStream=socket.getInputStream();
+                    byte[] bytes = new byte[1024];
+                    inputStream.read(bytes);
+                    String string = new String(bytes);
+                    System.out.println(string);
+
+                    //向客户端发送消息
+                    outputStream = socket.getOutputStream();
+                    outputStream.write("OK".getBytes());
+                    System.out.println("OK");
+
                 }
             } catch (Exception e) {
-                System.err.println(e);//也可以写成e.printStackTrace();
+                System.out.println("客户端主动断开连接了");
+                //e.printStackTrace();
             }
-
+            //操作结束，关闭socket
+            try{
+                socket.close();
+            }catch(IOException e){
+                System.out.println("关闭连接出现异常");
+                e.printStackTrace();
+            }
         }
     }
 }
